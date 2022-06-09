71afac472f072c73de217c271624a8c0723bd1b1
Paul Friedman <paulfri@hey.com>
2022-06-09 13:22:42 -0700

    Endpoints for days of week

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -22,6 +23,11 @@ func main() {
 		c.Redirect(http.StatusFound, randomCrosswordURL())
 	})
 
+	r.GET("/:dow", func(c *gin.Context) {
+		dow := c.Param("dow")
+		c.Redirect(http.StatusFound, randomCrosswordURLByDayOfWeek(dow))
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -36,6 +42,39 @@ func randomCrosswordURL() string {
 	dateSegment := dateString(date)
 
 	return fmt.Sprintf(format, dateSegment)
+}
+
+func randomCrosswordURLByDayOfWeek(dow string) string {
+	if !isValidDow(dow) {
+		panic("Invalid weekday")
+	}
+
+	var generatedDow string
+	var date time.Time
+	for ok := true; ok; ok = (strings.ToLower(generatedDow) != strings.ToLower(dow)) {
+		date = randomDate()
+		generatedDow = date.Format("Mon")
+		log.Println(generatedDow)
+	}
+
+	dateSegment := dateString(date)
+
+	return fmt.Sprintf(format, dateSegment)
+}
+
+func isValidDow(dow string) bool {
+	switch strings.ToLower(dow) {
+	case
+		"mon",
+		"tue",
+		"wed",
+		"thu",
+		"fri",
+		"sat",
+		"sun":
+		return true
+	}
+	return false
 }
 
 func randomDate() time.Time {
